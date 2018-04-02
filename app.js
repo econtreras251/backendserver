e634074dbb1f11ed6d653dd33e7894469059761d
Edgar Contreras <ec542498@gmail.com>
2018-04-02 15:37:12 -0430

    Agregando prototipo del TO-DO

--- app.js
+++ app.js
@@ -3,23 +3,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-
-
 // Inicializar variables
 const app = express()
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json())
 
-
 // Importar Rutas
 const appRoutes = require('./routes/app')
 const usuarioRoutes = require('./routes/usuario')
 const loginRoutes = require('./routes/login')
-
+const ticketsRoutes = require('./routes/ticket')
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',( err, res )=>{
-  //Si tiene un erro se dispara
+  //Si tiene un error se dispara
   if ( err ) throw err
   // Mensaje con expresion regular
   console.log('Base de Datos, port 27017: \x1b[32m%s\x1b[0m','Online');
@@ -27,9 +24,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',( err, res )=
 
 // Middleware
 app.use('/usuario', usuarioRoutes)
+app.use('/tickets', ticketsRoutes)
 app.use('/login', loginRoutes)
 app.use('/', appRoutes)
-
 
 // Escuchar peticiones
 app.listen(3000, ()=>{
---
+++ models/ticket.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const shortid = require('shortid')
+const Schema = mongoose.Schema
+
+const ticketSchema = new Schema({
+
+  titulo: { type: String, required: [true, 'El titulo es necesario'] },
+  descripcion: { type: String, required: [true, 'La descripcion debe tener algo'] },
+  id_validator: { type: String, unique:true, default: shortid.generate() },
+  user_send: { type: Schema.ObjectId, required: true, ref: 'Usuario' },
+  support_asigned: { type: Schema.ObjectId, required: true, ref: 'Usuario' },
+  status: { type: Boolean, default:false }  
+
+})
+
+module.exports = mongoose.model('Ticket', ticketSchema)
